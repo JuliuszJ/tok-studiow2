836a53ca2fa3ddec08a1044c63e8959ffb6171c0
Juliusz Jezierski <juliusz.jezierski@put.poznan.pl>
2025-11-15 19:32:34 +0100

    rest2

--- zaliczenie-semestru-srv/src/test/java/pl/dziekanat/zaliczenie_semestru_srv/PaymentServiceTest.java
+++ zaliczenie-semestru-srv/src/test/java/pl/dziekanat/zaliczenie_semestru_srv/PaymentServiceTest.java
@@ -1,4 +1,17 @@
 package pl.dziekanat.zaliczenie_semestru_srv;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class PaymentServiceTest {
+    @Test
+    void chargeTest(){
+        PaymentService.Payment payment = new PaymentService.Payment( "987", 999);
+        PaymentService paymentService = new PaymentService();
+        ResponseEntity<PaymentService.PaymentStatus> paymentStatus = paymentService.charge(payment);
+        Assertions.assertEquals(HttpStatus.OK, paymentStatus.getStatusCode());
+        Assertions.assertEquals("ABC321", paymentStatus.getBody().getTransId());
+    }
 }
